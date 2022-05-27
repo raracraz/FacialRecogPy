9f354447839e627948c9082c73878b8d0bde5d68
raynac <ryan.aaixe@gmail.com>
2022-05-27 19:30:20 +0800

    complete

--- main.py
+++ main.py
@@ -13,8 +13,23 @@ video = cv2.VideoCapture(0)
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
 while True:
-    event, values = window.read()
-    if event == sg.WIN_CLOSE:
+    event, values = window.read(timeout = 0)
+    if event == sg.WIN_CLOSED:
         break
 
+    _, frame = video.read()
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, scaleFactor = 1.5, minNeighbors = 5, minSize = (50, 50))
+
+    # draw the rectangle around the faces
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+        
+
+    #update the image
+    imgbytes = cv2.imencode('.png', frame)[1].tobytes()
+    window['-IMAGE-'].update(data = imgbytes)
+
+    #update the text
+    window['-TEXT-'].update(f'People in picture: {len(faces)}')
 window.close()
